e26dd07cc416dfedb60c2f6e8eb12c924a6e222e
BagelBro11 <mcdougalln@limestone.on.ca>
2023-05-24 08:42:55 -0400

    Print method
    added print method

--- BattleShip/src/App.java
+++ BattleShip/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        prn("Hello, World!");
     }
+
+    
+    
+    public static void prn(String text) throws InterruptedException {
+        for (int i = 0; i < text.length(); i++) {
+            System.out.print(text.charAt(i));
+            Thread.sleep(50);
+        } // this loop gives the typewriter effect for by printing each character and then
+          // thread.sleep to show pause
+        System.out.println("\n"); // new line
+    }
+
+
 }
